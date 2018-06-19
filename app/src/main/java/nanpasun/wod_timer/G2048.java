4da6046112cadfd00fcb65edd8bbf5de0b71c951
estgames <wkzkfmxk23@gmail.com>
2018-06-19 17:51:53 +0900

    2048 up down

--- app/src/main/java/nanpasun/wod_timer/G2048.java
+++ app/src/main/java/nanpasun/wod_timer/G2048.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class G2048 {
     int size;
     ArrayList<ArrayList<Integer>> board;
+    ArrayList<ArrayList<Integer>> undoBoard;
     Random random = new Random();
     int startNum = 2;
 
@@ -26,57 +27,112 @@ public class G2048 {
     }
 
     public Boolean gameOverCheck() {
+        if (boardFullFillCheck()) {
+
+        }
+
         return false;
     }
 
-    public void createNum() {
-        int x = random.nextInt(size);
-        int y = random.nextInt(size);
-
-        while (true) {
-            if (board.get(x).get(y).equals(0)) {
-                board.get(x).set(y, startNum);
-                break;
+    public Boolean gameClearCheck() {
+        for (ArrayList<Integer> list : this.board) {
+            for (Integer num : list) {
+                if (num.equals(2048))
+                    return true;
             }
-            x = random.nextInt(size);
-            y = random.nextInt(size);
+        }
+        return false;
+    }
+
+    public Boolean boardFullFillCheck() {
+        for (ArrayList<Integer> list : this.board) {
+            for (Integer num : list) {
+                if (num.equals(0))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public void createNum() {
+        createNum(1);
+    }
+
+    public void createNum(int num) {
+        if (num > 0 && !boardFullFillCheck()) {
+            int x, y;
+
+            while (true) {
+                x = random.nextInt(size);
+                y = random.nextInt(size);
+
+                if (board.get(x).get(y).equals(0)) {
+                    board.get(x).set(y, startNum);
+                    break;
+                }
+            }
+            createNum(--num);
         }
     }
 
-    public void upMove() {
-
-    }
-
-    public void downMove() {
-
-    }
-
-    private void setValueBoardValueIf0Elae(ArrayList<Integer> board, int i, Runnable _for) {
-        if (!board.get(i).equals(0)) {
+    private void setValueNotEmpty(Integer num, Runnable _for) {
+        if (!num.equals(0)) {
             _for.run();
         }
     }
 
-    private void setValueBoardMove(ArrayList<Integer> board, int a, int b) {
-        if (board.get(a).equals(0)) {
-            board.set(a, board.get(b));
-            board.set(b, 0);
-        } else if (board.get(a).equals(board.get(b))) {
-            board.set(a, board.get(a) + board.get(b));
-            board.set(b, 0);
+    private void setValueMoveAnd0Fill(ArrayList<Integer> board, int currentValue, int beforeValue) {
+        if (board.get(currentValue).equals(0)) {
+            board.set(currentValue, board.get(beforeValue));
+            board.set(beforeValue, 0);
+        } else if (board.get(currentValue).equals(board.get(beforeValue))) {
+            board.set(currentValue, board.get(currentValue) + board.get(beforeValue));
+            board.set(beforeValue, 0);
+        }
+    }
+
+    private void setValueMoveAnd0Fill(int cRowNum, int bRowNum, int currentValue, int beforeValue) {
+        if (board.get(cRowNum).get(currentValue).equals(0)) {
+            board.get(cRowNum).set(currentValue, board.get(bRowNum).get(beforeValue));
+            board.get(bRowNum).set(beforeValue, 0);
+        } else if (board.get(cRowNum).get(currentValue).equals(board.get(bRowNum).get(beforeValue))) {
+            board.get(cRowNum).set(currentValue, board.get(cRowNum).get(currentValue) + board.get(bRowNum).get(beforeValue));
+            board.get(bRowNum).set(beforeValue, 0);
+        }
+    }
+
+    public void upMove() {
+        for (int i = 1; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if(!board.get(i).get(j).equals(0)) {
+                    for (int z = i - 1; z >= 0; z--) {
+                        setValueMoveAnd0Fill(z, z+1, j, j);
+                    }
+                }
+            }
+        }
+    }
+
+    public void downMove() {
+        for (int i = size - 2; i > 0; i--) {
+            for (int j = 0; j < size; j++) {
+                if(!board.get(i).get(j).equals(0)) {
+                    for (int z = i + 1; z < size; z++) {
+                        setValueMoveAnd0Fill(z, z-1, j, j);
+                    }
+                }
+            }
         }
     }
 
     public void leftMove() {
-        for(final ArrayList<Integer> board1th : board) {
+        for (final ArrayList<Integer> board1th : board) {
             for (int i = 1; i < size; i++) {
                 final int finalI = i;
-                setValueBoardValueIf0Elae(board1th, i, new Runnable() {
-                    @Override
-                    public void run() {
-                        for (int j = finalI - 1; j >= 0; j--) {
-                            setValueBoardMove(board1th, j , j + 1);
-                        }
+
+                setValueNotEmpty(board1th.get(i), () -> {
+                    for (int j = finalI - 1; j >= 0; j--) {
+                        setValueMoveAnd0Fill(board1th, j , j + 1);
                     }
                 });
             }
@@ -84,15 +140,19 @@ public class G2048 {
     }
 
     public void rightMove() {
-        for(ArrayList<Integer> board1th : board) {
+        for (ArrayList<Integer> board1th : board) {
             for (int i = size - 2; i >= 0; i--) {
                 if (!board1th.get(i).equals(0)) {
                     for (int j = i + 1; j < size; j++) {
-                        setValueBoardMove(board1th, j , j - 1);
+                        setValueMoveAnd0Fill(board1th, j , j - 1);
                     }
                 }
             }
         }
+    }
+
+    public void undo() {
+
     }
 
     public void print() {
--- app/src/test/java/nanpasun/wod_timer/G2048Test.java
+++ app/src/test/java/nanpasun/wod_timer/G2048Test.java
@@ -2,40 +2,36 @@ package nanpasun.wod_timer;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 
 public class G2048Test {
     @Test
     public void initTest() {
         G2048 g2048 = new G2048(5);
-        g2048.createNum();
-        g2048.createNum();
-        g2048.createNum();
-        g2048.createNum();
-        g2048.createNum();
+        g2048.createNum(5);
         g2048.print();
         System.out.println();
         g2048.leftMove();
         g2048.print();
         System.out.println();
 
-        g2048.createNum();
-        g2048.createNum();
-        g2048.createNum();
-        g2048.createNum();
+        g2048.createNum(4);
         g2048.print();
         System.out.println();
-        g2048.leftMove();
+        g2048.downMove();
         g2048.print();
         System.out.println();
 
-        g2048.createNum();
-        g2048.createNum();
-        g2048.createNum();
-        g2048.createNum();
+        g2048.createNum(4);
         g2048.print();
         System.out.println();
         g2048.rightMove();
+        g2048.print();
+        System.out.println();
+
+        g2048.createNum(4);
+        g2048.print();
+        System.out.println();
+        g2048.upMove();
         g2048.print();
         System.out.println();
     }
