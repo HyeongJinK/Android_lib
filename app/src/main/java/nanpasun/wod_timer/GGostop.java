def3520f7a7fb1816526f588f1fa51fa0b161a31
estgames <wkzkfmxk23@gmail.com>
2018-06-25 17:53:55 +0900

    고스톱

--- app/src/main/java/nanpasun/wod_timer/GGostop.java
+++ app/src/main/java/nanpasun/wod_timer/GGostop.java
@@ -2,6 +2,7 @@ package nanpasun.wod_timer;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.function.Function;
 
 public class GGostop {
     enum CardType {
@@ -20,12 +21,22 @@ public class GGostop {
             this.type = type;
             this.month = month;
         }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Card) {
+                if (((Card) obj).type.equals(type) && ((Card) obj).month.equals(month)) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
     private ArrayList<Card> cards = new ArrayList<Card>();
     private ArrayList<Integer> randomNumArr = new ArrayList<Integer>();
     private int index = 0;
-    public ArrayList<Integer> backgroud = new ArrayList<Integer>();
+    public ArrayList<Card> backgroud = new ArrayList<Card>();
 
     GGostop () {
         cards.add(new Card(CardType.GWANG, CardMonth.JAN));
@@ -81,8 +92,9 @@ public class GGostop {
 
         Shuffle();
     }
-
-    private ArrayList<Integer> Shuffle() {
+    //카드섞기
+    public ArrayList<Integer> Shuffle() {
+        index = 0;
         randomNumArr.clear();
         ArrayList<Integer> tempNumArr = new ArrayList<Integer>();
 
@@ -101,15 +113,57 @@ public class GGostop {
 
         return randomNumArr;
     }
-
-    public Card getCard() {
+    //보드 세팅
+    public void backgroudSetting(int cardCount) {
+        //TODO 카드가 4장일때.
+        for (int i = 0; i < cardCount; i++)
+            backgroud.add(cards.get(randomNumArr.get(index++)));
+    }
+    //카드 드로우
+    public Card draw() {
         return cards.get(randomNumArr.get(index++));
     }
+    //
+    //private
+    //카드 비교
+    public ArrayList<Card> backgroudCompareCard(Card card
+        , Function<ArrayList<Card>, Card> matchCardTwo) {
+        ArrayList<Card> returnCards = new ArrayList<Card> ();
+        ArrayList<Card> matchCards = new ArrayList<Card>();
 
-    public void backgroudSetting(int cardCount) {
-        for (int i = 0; i < cardCount; i++)
-            backgroud.add(randomNumArr.get(index++));
+        if (card != null) {
+            for (Card back : backgroud) {
+                if (back.equals(card)) { //맞는 카드가 있을 경우
+                    matchCards.add(back);
+                }
+            }
+        }
+
+        switch (matchCards.size()) {
+            case 0: //맞는 카드가 없을 경우 보드에 추가한다.
+                backgroud.add(card);
+                break;
+            case 1: //맞는 카드가 한장일 경우 낸 카드와 맞는 카드를 가져가고 보드에서 없앤다.
+                returnCards.add(card);
+                returnCards.add(matchCards.get(0));
+                backgroud.remove(matchCards.get(0));
+                break;
+            case 2://TODO 두장일 경우 선택
+                Card choiceCard = matchCardTwo.apply(matchCards);
+                break;
+            case 3://TODO 세장일 경우 전부 가져가고 피 한장씩 받기
+                break;
+        }
+
+        return returnCards;
     }
+    //카드 뒤집기
+    public ArrayList<Card> upsideDownCard(Card card) {
+        return new ArrayList<Card> ();
+    }
+    //폭탄
 
-    //public
+
+
+
 }
--- app/src/test/java/nanpasun/wod_timer/GGostopTest.java
+++ app/src/test/java/nanpasun/wod_timer/GGostopTest.java
@@ -2,6 +2,7 @@ package nanpasun.wod_timer;
 
 import android.util.Log;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,12 +10,20 @@ import java.util.ArrayList;
 import static junit.framework.Assert.assertEquals;
 
 public class GGostopTest {
+
+
+//    @BeforeClass
+//    public void beforeClass() {
+//
+//    }
+
     @Test
     public void ShuffleTest() {
-        GGostop gostop = new GGostop();
-
+        GGostop gostop;
+        gostop = new GGostop();
         ArrayList<Integer> numArr = gostop.Shuffle();
 
+        assertEquals("카드 장수",50, numArr.size());
         for (Integer n : numArr) {
             System.out.println(n);
         }
